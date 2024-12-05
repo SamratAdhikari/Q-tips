f0d3c491b812a8442e3b2ecdf0be1fc28aadf1eb
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-12-05 14:08:50 +0545

    update height

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -18,7 +18,8 @@ const Home = () => {
 
     return (
         <div className="flex flex-col items-center justify-center h-full lg:w-[50%] sm:w-[70%] w-full mx-auto py-[8rem]">
-            <div className="w-full h-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+            {/* Main blurry section */}
+            <div className="w-full h-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 relative overflow-hidden">
                 <h1 className="text-3xl font-semibold text-center text-[#646EE4] mb-6">
                     Q-tips
                     <span className="text-gray-300 ml-4">
@@ -27,8 +28,9 @@ const Home = () => {
                 </h1>
 
                 <form onSubmit={handleSubmit}>
+                    {/* Context Input */}
                     <div>
-                        <label className="label p-2 ">
+                        <label className="label p-2">
                             <span className="label-text text-lg font-semibold text-gray-400">
                                 Context
                             </span>
@@ -41,6 +43,7 @@ const Home = () => {
                         ></textarea>
                     </div>
 
+                    {/* Answer Input */}
                     <div>
                         <label className="label p-2 mt-4">
                             <span className="text-lg label-text font-semibold text-gray-400">
@@ -56,6 +59,7 @@ const Home = () => {
                         />
                     </div>
 
+                    {/* Submit Button */}
                     <div className="flex items-center justify-center">
                         <button className="btn btn-active btn-primary my-6 text-base py-2 min-w-[128px]">
                             {loading ? (
@@ -67,6 +71,7 @@ const Home = () => {
                     </div>
                 </form>
 
+                {/* Question Section */}
                 {question && (
                     <div className="mt-4">
                         <label className="label p-2">
